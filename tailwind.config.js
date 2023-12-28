691826137f2f67f3e8bd788c77d621b19f964027
eduxal <eduardo.rual58@gmail.com>
2023-12-28 02:08:55 -0800

    first config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'e78b90': '#e78b90',
+      },
+    },
+  },
+  plugins: [],
+}
+
